90888aaf3db5faa14869b912c38c056760d0a903
Joe Hildebrand <joe-github@cursive.net>
2022-03-02 13:58:20 -0700

    Add key signature on mouseover

--- random.js
+++ random.js
@@ -1,32 +1,45 @@
-export const majors = [
-  'C',
-  'F',
-  'B&#9837;',
-  'E&#9837;',
-  'A&#9837;',
-  'C&#9839;',
-  'F&#9839;',
-  'B',
-  'E',
-  'A',
-  'D',
-  'G',
+const DISPLAY = {
+  'b': '&#9837;',
+  '#': '&#9839;',
+}
+const FILE = {
+  'b': '-flat',
+  '#': '-sharp',
+}
+
+class Key {
+  constructor(major, minor, url, ...alts) {
+    this.major = Key.pretty(major, DISPLAY);
+    this.minor = `<b>${Key.pretty(minor, DISPLAY)}</b> min`;
+    this.url = url
+    this.alts = [major, ...alts]
+  }
+
+  static pretty(key, map) {
+    return key[0] + (map[key[1]] || '')
+  }
+}
+
+const P = 'https://upload.wikimedia.org/wikipedia/commons/'
+const KEYS = [
+  new Key('C', 'A', `${P}3/33/C-major_a-minor.svg`, 'B#'),
+  new Key('F', 'D', `${P}b/b4/F-major_d-minor.svg`, 'E#'),
+  new Key('Bb', 'G', `${P}f/fe/B-flat-major_g-minor.svg`, 'A#'),
+  new Key('Eb', 'C', `${P}c/cf/E-flat-major_c-minor.svg`, 'D#'),
+  new Key('Ab', 'F', `${P}0/0b/A-flat-major_f-minor.svg`, 'G#'),
+  // https://upload.wikimedia.org/wikipedia/commons/2/2d/C-sharp-major_a-sharp-minor.svg
+  new Key('Db', 'Bb', `${P}8/87/D-flat-major_b-flat-minor.svg`, 'C#'),
+  // https://upload.wikimedia.org/wikipedia/commons/3/3d/F-sharp-major_d-sharp-minor.svg
+  new Key('Gb', 'Eb', `${P}7/76/G-flat-major_e-flat-minor.svg`, 'F#'),
+  new Key('B', 'G#', `${P}6/65/B-major_g-sharp-minor.svg`, 'Cb'),
+  new Key('E', 'C#', `${P}f/f3/E-major_c-sharp-minor.svg`, 'Fb'),
+  new Key('A', 'F#', `${P}8/84/A-major_f-sharp-minor.svg`),
+  new Key('D', 'B', `${P}d/d3/D-major_b-minor.svg`),
+  new Key('G', 'E', `${P}1/13/G-major_e-minor.svg`),
 ]
 
-export const minors = [
-  '<b>A</b> min',
-  '<b>D</b> min',
-  '<b>G</b> min',
-  '<b>C</b> min',
-  '<b>F</b> min',
-  '<b>B&#9837;</b> min',
-  '<b>E&#9837;</b> min',
-  '<b>A&#9837;</b> min',
-  '<b>C&#9839;</b> min',
-  '<b>F&#9839;</b> min',
-  '<b>B</b> min',
-  '<b>E</b> min',
-]
+export const majors = KEYS.map(orig => ({key: orig.major, orig}))
+export const minors = KEYS.map(orig => ({key: orig.minor, orig}))
 
 function zip(a, b) {
   return a.flatMap((_, i) => [a[i], b[i]])
@@ -36,13 +49,24 @@ export const all = zip(majors, minors)
 
 function shuffle(a) {
   return a
-    .map(key => ({ key, sort: Math.random() }))
+    .map(({key, orig}) => ({ key, orig, sort: Math.random() }))
     .sort((a, b) => a.sort - b.sort)
-    .map(({ key }) => key)
 }
 
 export default function random() {
   return zip(shuffle(majors), shuffle(minors))
+}
+
+function mouseIn(ev) {
+  const img = document.getElementById('keysignature')
+  img.src = ev.target.orig.url
+  ev.target.classList.add('has-text-info')
+}
+
+function mouseOut(ev) {
+  const img = document.getElementById('keysignature')
+  img.src = ''
+  ev.target.classList.remove('has-text-info')
 }
 
 function fill(scales) {
@@ -57,10 +81,13 @@ function fill(scales) {
   let value = 1
   for (const [i, ol] of ols.entries()) {
     ol.innerText = ''
-    for (const scale of chunks[i]) {
+    for (const {key, orig} of chunks[i]) {
       const li = document.createElement('li')
       li.value = value++
-      li.innerHTML = scale
+      li.innerHTML = key
+      li.onmouseenter = mouseIn
+      li.onmouseleave = mouseOut
+      li.orig = orig
       ol.appendChild(li)
     }
   }
